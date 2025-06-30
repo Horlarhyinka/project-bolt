92da0dfdf2f2c1b517d8821e481fda40d65623e7
Horlarhyinka <danijufarouq2003@gmail.com>
2025-06-30 16:47:37 +0100

    revert cors update

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,9 +17,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors({
-  origin: '*',
+  origin: envVars.CLIENT_BASE_URL, // e.g., 'http://localhost:3000'
   credentials: true
 }))
+console.log('My cors origin points to ->', envVars.CLIENT_BASE_URL)
 app.use(Session({ 
     secret: envVars.APP_SECRET, 
     resave: true,
